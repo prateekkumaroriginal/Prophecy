0a4e0b8b97d5d19889d8f1d776ab9a33ae3505f8
Prateek <prateek.kumar.original@gmail.com>
2023-11-27 00:20:03 +0530

    Implement logout

--- prophecy_project/user/urls.py
+++ prophecy_project/user/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('login/', views.user_login, name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='user/logout.html'), name='logout'),
 ]
