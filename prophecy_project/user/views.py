a36936999d4b7cbc70ec0848df80b0b255c4d660
Prateek <prateek.kumar.original@gmail.com>
2023-11-27 00:07:49 +0530

    Implement login

--- prophecy_project/user/views.py
+++ prophecy_project/user/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login
 from .froms import LoginForm
+from django.http import HttpResponse
 
 # Create your views here.
-def login(request):
-    form = LoginForm()
+def user_login(request):
+    if request.method=='POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            user = authenticate(request, username=data['username'], password=data['password'])
+            if user:
+                login(request, user)
+                return HttpResponse('User Authenticated')
+            else:
+                return HttpResponse('Invalid Login')
+    else:
+        form = LoginForm()
     return render(request, 'user/login_form.html', {'form': form})
-    
