491c537e3491a6304a8f3e7f8230449400c4911f
Prateek <prateek.kumar.original@gmail.com>
2023-11-27 14:00:19 +0530

    Reset Password

--- prophecy_project/user/urls.py
+++ prophecy_project/user/urls.py
@@ -3,8 +3,13 @@ from . import views
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
+    path("", views.index, name="index"),
     path('login/', views.user_login, name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='user/logout.html'), name='logout'),
     path('password_change/', auth_views.PasswordChangeView.as_view(template_name='user/password_change.html'), name='password_change'),
     path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(template_name='user/password_change_done.html'), name='password_change_done'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='user/password_reset_form.html'), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='user/password_reset_done.html'), name='password_reset_done'),
+    path('password_reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='user/password_reset_confirm.html'), name='password_reset_confirm'),
+    path('password_reset/complete/', auth_views.PasswordResetCompleteView.as_view(template_name='user/password_reset_complete.html'), name='password_reset_complete'),
 ]
