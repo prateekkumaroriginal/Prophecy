6c14cd7ed597fbd04c4794bae1224c8928cbae4b
Prateek <prateek.kumar.original@gmail.com>
2023-11-28 18:33:23 +0530

    Edit user profile

--- prophecy_project/user/views.py
+++ prophecy_project/user/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
-from .forms import LoginForm, UserRegistrationForm
+from .forms import LoginForm, UserRegistrationForm, UserEditForm, ProfileEditForm
 from django.http import HttpResponse
+from .models import Profile
 
 # Create your views here.
 def user_login(request):
@@ -31,7 +32,24 @@ def register(request):
             new_user = user_form.save(commit=False)
             new_user.set_password(user_form.cleaned_data['password'])
             new_user.save()
+            Profile.objects.create(user=new_user)
             return render(request, 'user/register_done.html')
     else:
         user_form = UserRegistrationForm()
     return render(request, 'user/register.html', {'user_form':user_form})
+
+@login_required
+def edit(request):
+    if request.method == 'POST':
+        user_form = UserEditForm(instance=request.user, data=request.POST)
+        profile_form = ProfileEditForm(instance=request.user.profile, data=request.POST, files=request.FILES)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+    else:
+        user_form = UserEditForm(instance=request.user)
+        profile_form = ProfileEditForm(instance=request.user.profile)
+    return render(request, 'user/profile_edit.html', {
+        'user_form': user_form,
+        'profile_form': profile_form,
+    })
