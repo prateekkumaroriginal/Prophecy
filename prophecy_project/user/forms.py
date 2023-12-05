445ae02f651f68f495040f52748ff5acabfd58a6
Prateek <prateek.kumar.original@gmail.com>
2023-12-05 15:53:57 +0530

    commit for check_password in forms.py

--- prophecy_project/user/forms.py
+++ prophecy_project/user/forms.py
@@ -8,8 +8,8 @@ class LoginForm(forms.Form):
     password = forms.CharField(widget=forms.PasswordInput)
 
 class UserRegistrationForm(forms.ModelForm):
-    password = forms.CharField(label='Password', widget=forms.PasswordInput)
-    password2 = forms.CharField(label='Confirm Password', widget=forms.PasswordInput)
+    password = forms.CharField(label='Password', widget=forms.PasswordInput())
+    password2 = forms.CharField(label='Confirm Password', widget=forms.PasswordInput())
     class Meta:
         model = User
         fields = ['username', 'email', 'first_name', 'last_name']
